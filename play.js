ff3359ca59cc1b6bb97232b9c684c36c02b07029
Jayesh Shukla <130957471+Jay-gif-ui@users.noreply.github.com>
2023-10-13 03:10:32 -0700

    my web-flow

---
+++ play.js
@@ -0,0 +1,9 @@
+function openmenu() {
+    var sidemenu = document.getElementById("sidemenu");
+    sidemenu.classList.add("active");
+}
+
+function closemenu() {
+    var sidemenu = document.getElementById("sidemenu");
+    sidemenu.classList.remove("active");
+}
